b8c3d5741ca4412235bd34c304ed970e14a52bec
Mike Lima <mike.lima@tegra.me>
2016-04-08 16:53:42 -0300

    Rendering HTML as Response

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 var http = require("http");
 var moduleone = require("./moduleone");
 var modulotwo = require("./moduletwo");
+var fs = require("fs");
+
 
 const onRequest = (request, response) => {
-  response.writeHead(200,{"Content-Type":"text/plain"});
-  response.write("hello word");
-  response.write(moduleone.myString);
-  moduleone.myFunction();
+  response.writeHead(200,{"Content-Type":"text/html"});
+  fs.readFile("./index.html",null,(err,data)=>{
+     console.log("wtf",data);
+     if (err){
+        response.writeHead(404);
+        response.write("File not Found");
+      }
+      else{
+        response.write(data);
+       }
+      response.end();
+  });
+  // response.write("index.html");
+  // response.write(moduleone.myString);
+  // moduleone.myFunction();
 
-  modulotwo.myOtherFunction();
-  console.log(modulotwo.myVariable);
-  response.end();
+  // modulotwo.myOtherFunction();
+  // console.log(modulotwo.myVariable);
+  // response.end();
 }
 
 
