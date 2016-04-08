55709c38665b3395b6da6888dfddd08eb9b16551
Mike Lima <mike.lima@tegra.me>
2016-04-08 17:33:49 -0300

    Routing

---
+++ app.js
@@ -0,0 +1,38 @@
+var url = require("url");
+var fs = require("fs");
+
+var renderHtml = (path, response) => {
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      response.writeHead(404);
+      response.write("File not Found");
+    }
+    else {
+      response.write(data);
+    }
+    response.end();
+
+  });
+}
+
+module.exports = {
+  handleRequest: (request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    var path = url.parse(request.url).pathname;
+
+    switch (path) {
+      case "/":
+        renderHtml("./index.html", response);
+        break;
+      case "/login":
+        renderHtml("./login.html", response);
+        break;
+      default:
+        response.writeHead(404)
+        response.write("Page not Found")
+        response.end();
+    }
+
+  }
+};
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 var http = require("http");
-var moduleone = require("./moduleone");
-var modulotwo = require("./moduletwo");
-var fs = require("fs");
+var app = require("./app");
 
 
-const onRequest = (request, response) => {
-  response.writeHead(200,{"Content-Type":"text/html"});
-  fs.readFile("./index.html",null,(err,data)=>{
-     console.log("wtf",data);
-     if (err){
-        response.writeHead(404);
-        response.write("File not Found");
-      }
-      else{
-        response.write(data);
-       }
-      response.end();
-  });
+
+http.createServer(app.handleRequest).listen(8000);
+
+// var moduleone = require("./moduleone");
+// var modulotwo = require("./moduletwo");
+// var fs = require("fs");
+
+
+// const onRequest = (request, response) => {
+//   response.writeHead(200,{"Content-Type":"text/html"});
+//   fs.readFile("./index.html",null,(err,data)=>{
+//      console.log("wtf",data);
+//      if (err){
+//         response.writeHead(404);
+//         response.write("File not Found");
+//       }
+//       else{
+//         response.write(data);
+//        }
+//       response.end();
+//   });
   // response.write("index.html");
   // response.write(moduleone.myString);
   // moduleone.myFunction();
@@ -24,7 +30,7 @@ const onRequest = (request, response) => {
   // modulotwo.myOtherFunction();
   // console.log(modulotwo.myVariable);
   // response.end();
-}
+//}
 
 
-http.createServer(onRequest).listen(8000);
+//http.createServer(onRequest).listen(8000);
