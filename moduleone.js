6d52141bd8e93ef1af017e39444e50015c86e98d
Mike Lima <mike.lima@tegra.me>
2016-04-08 14:15:07 -0300

    My IDE

--- moduleone.js
+++ moduleone.js
@@ -2,7 +2,7 @@ const myFunction  = () => {
   console.log("Function Was Called");
 }
 
-var myString = "My Stringona!";
+var myString = "My Stringona!!!!";
 
 module.exports.myFunction = myFunction;
 module.exports.myString = myString;
