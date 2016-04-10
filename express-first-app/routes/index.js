21a259653717160b4a552acb08ced408668213ed
mikemajesty <mikemajesty2011@gmail.com>
2016-04-10 13:27:56 -0300

    JADE Templating Engine

--- express-first-app/routes/index.js
+++ express-first-app/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',condition: false});
+});
+
+router.get('/users',function(req, res, next){
+    res.send('response with user');
+});
+ 
+router.get('/users/detail',function(req, res, next){
+    res.send('response with user Detail');
 });
 
 module.exports = router;
